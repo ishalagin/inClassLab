eca57f53c971088ee74992f6fe4c71683541b507
ishalagin <i.shalagin@gmail.com>
2019-10-03 10:12:56 -0700

    testing

--- inClassLab/Controllers/HomeController.cs
+++ inClassLab/Controllers/HomeController.cs
@@ -20,11 +20,6 @@ namespace inClassLab.Controllers
             return View();
         }
 
-        public IActionResult Nam()
-        {
-            return View();
-        }
-
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
